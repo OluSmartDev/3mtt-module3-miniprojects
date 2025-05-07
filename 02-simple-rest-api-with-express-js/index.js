068055945a278a062da405f4345b3e7967f7c21b
OluSmartDev <oolalere2015@gmail.com>
2025-05-07 19:41:37 +0100

    Added itemsRouter.js

--- 02-simple-rest-api-with-express-js/index.js
+++ 02-simple-rest-api-with-express-js/index.js
@@ -1,5 +1,12 @@
 import express from "express";
+import itemsRouter from "./routes/itemsRouter.js";
 const app = express();
 const PORT = 5000;
+
+app.use(express.json());
+
+app.use('/items', itemsRouter)
+
+app.get('/', (req, res) => res.send("Hello, World!"));
 
 app.listen(PORT, () => console.log(`SERVER is running at http://localhost:${PORT}`));
---
+++ 02-simple-rest-api-with-express-js/routes/itemsRouter.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const router = express.Router();
+
+// router.get('/', getItems)
+
+// router.get('/:id', getItem)
+
+// router.post('/', createItem)
+
+// router.put('/:id', updateItem)
+
+// router.delete('/:id', deleteItem)
+
+export default router;
+
+
