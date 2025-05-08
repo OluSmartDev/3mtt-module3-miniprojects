00eb98a8e2ed495f78ad545d6b871e7d4fb9b2f0
OluSmartDev <oolalere2015@gmail.com>
2025-05-08 16:02:19 +0100

    Implemented getItems and createItem without error handling

--- 02-simple-rest-api-with-express-js/controllers/itemsController.js
+++ 02-simple-rest-api-with-express-js/controllers/itemsController.js
@@ -1,9 +1,27 @@
 import {v4 as uuidv4} from 'uuid';
 import {param, body, validationResult} from 'express-validator';
 
-export const getItems = () => {}
+let items = [];
+
+// --- Helper function to handle errors ---
+const handleError = (res, status, message, details = null) => {
+    console.error(`HTTP ${status} - ${message}`, details || '');
+    return res.status(status).json({ error: message, details });
+  };
+
+export const getItems = (req, res) => {
+    res.json(items);
+}
 
 export const getItem = () => {}
-export const createItem = () => {}
+
+export const createItem = (req, res) => {
+    const item = {id: uuidv4(), ...req.body};
+    console.log(item);
+    items.push(item);
+    res.status(200).send("Item created successfully")
+}
+
 export const updateItem = () => {}
+
 export const deleteItem = () => {}
